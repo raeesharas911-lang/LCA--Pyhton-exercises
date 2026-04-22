b7a77e28e1a8f13737ea384633d5d4a8941f449e
raeesharas911-lang <raeesharas911@gmail.com>
2026-04-22 19:32:45 +0200

    Add exercise 02 - Operators and conditional logic

---
+++ week_03_ex02_operators.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 5
+
+# Arithmetic Operators
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Comparison Operators
+print("Is num1 greater than num2?", num1 > num2)
+print("Is num1 equal to num2?", num1 == num2)
+
+# Conditional Logic
+if num1 > num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater or equal to num1")
