07f17ba11705eb62bb64c435c68900727e880ccb
raeesharas911-lang <raeesharas911@gmail.com>
2026-04-23 22:06:38 +0200

    Add Exercise03_ - Functions

--- week3_ex03_functions.py
+++ week3_ex03_functions.py
@@ -1,4 +1,29 @@
-def hello():
-    print(""hello)
+def greet():
+    print("Hello World!")
+    greet()
 
-    
+def personalized_greeting(name):
+    print(f"Hello {name}")
+personalized_greeting("Raeesha")
+
+def square(number):
+    return number * number
+result = square(5)
+print(result)
+
+def rectangle_area(length, width):
+    return length * width
+area = rectangle_area(4, 5)
+print(area)
+
+def apply_operation(func, number):
+    return func(number)
+
+def apply_operation(func, number):
+    return func(number)
+
+def double(number):
+    return number * 2
+
+print(apply_operation(double, 7))
+print(apply_operation(square, 3))
